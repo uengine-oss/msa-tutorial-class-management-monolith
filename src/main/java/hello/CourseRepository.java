4a754ed2abf038fda077a4a017290d6de79ea002
Jinyoung Jang <jyjang@uengine.org>
2018-03-11 19:24:18 +0900

    Update CourseRepository.java

--- src/main/java/hello/CourseRepository.java
+++ src/main/java/hello/CourseRepository.java
@@ -2,9 +2,6 @@ package hello;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-/**
- * Created by uengine on 2018. 1. 6..
- */
-//@RepositoryRestResource
+@RepositoryRestResource
 public interface CourseRepository extends PagingAndSortingRepository<Course, Long> {
 }
